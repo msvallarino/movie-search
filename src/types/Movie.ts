2ab054072298399b48d9a0491f2499d031a51e23
mvallarino <matias.vallarino@globant.com>
2024-02-28 17:42:27 -0500

    Add types

---
+++ src/types/Movie.ts
@@ -0,0 +1,9 @@
+export type Movie = {
+    id: number;
+    title: string;
+    overview: string;
+    original_title: string;
+    popularity: number;
+    release_date: string;
+    poster_path: string;
+  }
